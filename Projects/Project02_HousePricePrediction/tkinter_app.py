c49dfd5138cc67a250c77490de8f7c42e46b45a7
GetPython2020 <72427707+GetPython2020@users.noreply.github.com>
2020-11-18 22:45:57 -0700

    Update tkinter_app.py

--- Projects/Project02_HousePricePrediction/tkinter_app.py
+++ Projects/Project02_HousePricePrediction/tkinter_app.py
@@ -1,43 +1,48 @@
 import tkinter as tk
 import json
 import model
-import numpy as np
 
 window = tk.Tk()
-window.geometry('500x500')
+window.geometry('500x600')
+canvas = tk.Canvas(width=500,height=500,bg='light blue')
+canvas.pack()
 
-lb_location = tk.Label(text='location',font=20)
-lb_location.grid(row=0,column=0)
-lb_area = tk.Label(text='area(sqft)',font=20)
-lb_area.grid(row=1,column=0)
-lb_bhk = tk.Label(text='bhk',font=20)
-lb_bhk.grid(row=2,column=0)
-lb_bath = tk.Label(text='bath',font=20)
-lb_bath.grid(row=3,column=0)
+lb_title = tk.Label(text='House Price Predict',font=25,fg='red')
+canvas.create_window(250,30,window=lb_title)
+
+lb_location = tk.Label(text='Location',font=20)
+canvas.create_window(100,100,window=lb_location)
+lb_area = tk.Label(text='Area(sqft)',font=20)
+canvas.create_window(100,150,window=lb_area)
+lb_bhk = tk.Label(text='Bhk',font=20)
+canvas.create_window(100,200,window=lb_bhk)
+lb_bath = tk.Label(text='Bath',font=20)
+canvas.create_window(100,250,window=lb_bath)
 
 with open('columns.json','r') as f:
     location_list = json.load(f)['data_columns'][4:]
 variable_loc = tk.StringVar()
 drmn_loc = tk.OptionMenu(window,variable_loc,location_list[0],*location_list[1:])
-drmn_loc.grid(row=0,column=1)
+canvas.create_window(300,100,window=drmn_loc)
 
 ent_area = tk.Entry()
-ent_area.grid(row=1,column=1)
+canvas.create_window(300,150,window=ent_area)
 
 bhk_list = [1,2,3,4,5,6]
 variable_bhk = tk.StringVar()
 drmn_bhk = tk.OptionMenu(window,variable_bhk,bhk_list[0],*bhk_list[1:])
-drmn_bhk.grid(row=2,column=1)
+canvas.create_window(300,200,window=drmn_bhk)
 
 bath_list = [1,2,3,4,5,6]
 variable_bath = tk.StringVar()
 drmn_bath = tk.OptionMenu(window,variable_bath,bath_list[0],*bath_list[1:])
-drmn_bath.grid(row=3,column=1)
+canvas.create_window(300,250,window=drmn_bath)
 
 
-drmn_loc.config(font=('calibri',(10)),bg='white',width=12)
-drmn_bhk.config(font=('calibri',(10)),bg='white',width=12)
-drmn_bath.config(font=('calibri',(10)),bg='white',width=12)
+drmn_loc.config(font=('calibri',(15)),bg='white',width=20)
+drmn_bhk.config(font=('calibri',(15)),bg='white',width=20)
+drmn_bath.config(font=('calibri',(15)),bg='white',width=20)
+ent_area.config(font=('calibri',(15)),bg='white',width=24)
 
 def click():
     global variable_loc
@@ -53,16 +58,16 @@ def click():
     result = model.predict_price(location,area,bhk,bath)
 
     lb_result = tk.Label(text='The estimated price is:',font=20)
-    lb_result.grid(row=5,column=0)
+    canvas.create_window(100,400,window=lb_result)
 
     lb_result2 = tk.StringVar()
     lb_result2.set('')
     lb_result2 = result
     lb_result3 = tk.Label(text=lb_result2,font=20)
-    lb_result3.grid(row=5, column=1)
+    canvas.create_window(300,400,window=lb_result3)
 
-btn_estimate = tk.Button(text='estimate',command=click)
-btn_estimate.grid(row=4,columnspan=2)
+btn_estimate = tk.Button(text='Estimate',command=click,font=15,fg='orange')
+canvas.create_window(250,300,window=btn_estimate)
 
 
 window.mainloop()
